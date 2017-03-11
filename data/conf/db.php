2da7b3e0b388c56d5bdf30f5b4493dc6f9918d9b
Powerless <wzxaini9@139.com>
2017-03-12 03:26:52 +0800

    本地数据上传

---
+++ data/conf/db.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * 配置文件
+ */
+return array(
+    'DB_TYPE' => 'mysql',
+    'DB_HOST' => 'localhost',
+    'DB_NAME' => 'ibtv_ru',
+    'DB_USER' => 'root',
+    'DB_PWD' => 'ibtv_rootmysql',
+    'DB_PORT' => '3306',
+    'DB_PREFIX' => 'i_',
+    //密钥
+    "AUTHCODE" => 'Uu2A4EWh7bWDXU19E9',
+    //cookies
+    "COOKIE_PREFIX" => 'wumC0Y_',
+);
+?>
